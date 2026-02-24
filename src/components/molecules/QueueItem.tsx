a000e00a3349bc6df23a175cff4ec6469be82ab3
Abhishek Maurya <mauryaabhishek279@gmail.com>
2026-02-25 00:25:29 +0530

    feat: added index to queue items

--- src/components/molecules/QueueItem.tsx
+++ src/components/molecules/QueueItem.tsx
@@ -1,16 +1,50 @@
 import Image from "next/image";
 import { Typography } from "@/components/atoms";
 
-export default function QueueItem({ item }: { item }) {
+interface Queue {
+  track: {
+    _id: string;
+    trackId: string;
+    title: string;
+    publisher: string;
+    thumbnail: string;
+    provider: string;
+  };
+}
+
+export default function QueueItem({
+  item,
+  index,
+}: {
+  item: Queue;
+  index: number;
+}) {
   return (
     <li>
-      <div className="flex items-center gap-2 bg-red-900 p-2 rounded-md">
-        <Image src={item.track.thumbnail} alt={item.track.title} width={48} height={48} />
-        <div>
-          <Typography variant="bodySmall" as="h3">{item.track.title}</Typography>
-          <Typography variant="bodySmall">{item.track.publisher}</Typography>
+      <div className="flex items-stretch bg-[#1A1B1E] text-white p-3 rounded-md gap-3">
+        <div className="flex flex-col">
+          <span className="mt-auto text-xl text-zinc-400 font-medium">
+            {String(index).padStart(2, "0")}
+          </span>
         </div>
-        {/* <div className="ml-auto">added by {item.addedBy}</div> */}
+
+        <div className="flex items-center gap-3">
+          <Image
+            src={item.track.thumbnail}
+            alt={item.track.title}
+            width={48}
+            height={56}
+            className="rounded-md"
+          />
+          <div>
+            <Typography variant="bodySmall" as="h3">
+              {item.track.title}
+            </Typography>
+            <Typography variant="bodySmall" className="text-zinc-400">
+              {item.track.publisher}
+            </Typography>
+          </div>
+        </div>
       </div>
     </li>
   );
