9766da1bcf520b11feebd2f58dff16133f78ceab
Abhishek Maurya <mauryaabhishek279@gmail.com>
2026-02-28 23:48:21 +0530

    fix: Better thumbnail rendering

--- src/components/molecules/QueueItem.tsx
+++ src/components/molecules/QueueItem.tsx
@@ -23,19 +23,24 @@ export default function QueueItem({
     <li>
       <div className="flex items-stretch bg-[#1A1B1E] text-white p-3 rounded-md gap-3">
         <div className="flex flex-col">
-          <Typography variant="bodySmall" as="span" className="text-zinc-400 font-medium mt-auto mb-1">
+          <Typography
+            variant="bodySmall"
+            as="span"
+            className="text-zinc-400 font-medium mt-auto mb-1"
+          >
             {String(index).padStart(2, "0")}
           </Typography>
         </div>
 
         <div className="flex items-center gap-3">
-          <Image
-            src={item.track.thumbnail}
-            alt={item.track.title}
-            width={48}
-            height={48}
-            className="rounded-md aspect-square"
-          />
+          <div className="w-12 aspect-square rounded-md overflow-hidden relative">
+            <Image
+              src={item.track.thumbnail}
+              alt={item.track.title}
+              fill
+              className="object-cover"
+            />
+          </div>
           <div>
             <Typography variant="bodySmall" as="h3" className="line-clamp-1">
               {item.track.title}
